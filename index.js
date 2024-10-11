9e5f1ded787dee79b5b0efbc5f365d0475989070
[Huleux Jérémy] <[huleux.jeremy@gmail.com]>
2024-10-11 17:45:42 +0200

    debut de modification de la partie portfolio, tentative d'ajout d'effets scroll GSAP, premiere modal terminé mais trop basique

--- index.js
+++ index.js
@@ -751,3 +751,51 @@ $(document).ready(function () {
       });
   });
 });
+
+
+/*
+  essaies effets GSAP
+*/
+function animateFrom(elem, direction) {
+  direction = direction || 1;
+  var x = 0,
+      y = direction * 100;
+  if(elem.classList.contains("gs_reveal_fromLeft")) {
+    x = -100;
+    y = 0;
+  } else if (elem.classList.contains("gs_reveal_fromRight")) {
+    x = 100;
+    y = 0;
+  }
+  elem.style.transform = "translate(" + x + "px, " + y + "px)";
+  elem.style.opacity = "0";
+  gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+    duration: 1.25, 
+    x: 0,
+    y: 0, 
+    autoAlpha: 1, 
+    ease: "expo", 
+    overwrite: "auto"
+  });
+}
+
+function hide(elem) {
+  gsap.set(elem, {autoAlpha: 0});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  gsap.registerPlugin(ScrollTrigger);
+  
+  // Appliquer l'animation aux sections de révélation
+  gsap.utils.toArray(".reveal-section").forEach(function(elem) {
+    hide(elem); // assure that the element is hidden when scrolled into view
+    
+    ScrollTrigger.create({
+      trigger: elem,
+      start: "top 75%", // Commencer l'animation lorsque la section entre dans la vue
+      onEnter: function() { animateFrom(elem) }, 
+      onEnterBack: function() { animateFrom(elem, -1) },
+      onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled out of view
+    });
+  });
+});
